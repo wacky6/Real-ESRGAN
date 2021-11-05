da864e7c1ee9949b5887d26243342ba99a948719
wacky6 <qjw@wacky.one>
2021-11-06 00:04:21 +1100

    add env-init script for ubuntu

---
+++ env-init-ubuntu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Assuming a nvidia/cuda cudnn runtime image.
+export DEBIAN_FRONTEND=noninteractive
+apt update
+
+apt install -y python3 python3-pip build-essential curl git libgl1-mesa-glx libglib2.0-0 ffmpeg
+
+# Assuming a Ampere GPU.
+pip3 install torch==1.10.0+cu111 torchvision==0.11.1+cu111 torchaudio==0.10.0+cu111 -f https://download.pytorch.org/whl/cu111/torch_stable.html
+
+pip3 install tqdm opencv-python
+pip3 install 'gfpgan>=0.2.1' 'facexlib>=0.2.0.3' 'basicsr>=1.3.3.11'
+python3 ./setup.py build
